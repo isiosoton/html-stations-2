31534774bdc86356d30927e0fd5e18df617bbbdf
kameoka <e2058232@soka-u.jp>
2023-07-12 10:22:47 +0900

    station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,8 +1,3 @@
 function changeBackgroundColor(e) {
   document.getElementById("text").style.backgroundColor = document.getElementById("check").checked ? "red" : "white";
-  // if (document.getElementById("check").checked) {
-  //   document.getElementById("text").style.backgroundColor = "red";
-  // } else {
-  //   document.getElementById("text").style.backgroundColor = "white";
-  // }
 }
